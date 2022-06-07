36539b88a268c0bf6b6e9ee25a7f5fa186ef0138
fhildeb <felix@lukso.io>
2022-06-07 23:36:53 +0200

    model: add item price

---
+++ practical-5/src/model/Einzelpreis.java
@@ -0,0 +1,25 @@
+package model;
+
+public class Einzelpreis {
+
+	public double betrag;
+	public String waehrung;
+	
+	
+	public Einzelpreis() {};
+	public Einzelpreis(double betrag, String waehrung) {
+	}
+	
+	public double getBetrag() {
+		return betrag;
+	}
+	public String getWaehrung() {
+		return waehrung;
+	}
+	public void setBetrag(double betrag) {
+		this.betrag = betrag;
+	}
+	public void setWaehrung(String waehrung) {
+		this.waehrung = waehrung;
+	}
+}
