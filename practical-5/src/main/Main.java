c2b105712526307bb1eb742a45049edbe965ce8b
fhildeb <felix@lukso.io>
2022-06-07 23:37:38 +0200

    add main function

---
+++ practical-5/src/main/Main.java
@@ -0,0 +1,36 @@
+package main;
+
+import model.Rechnungsliste;
+
+public class Main {
+
+	public static void main(String[] ars)
+	{
+		String pfad = "./Elektronikrechnungen_Daten_falsch.json";
+		
+		JsonTester test = new JsonTester(pfad);
+		
+		Rechnungsliste liste = test.leseJSONinRechnungsliste();
+		test.zeigeRechnungslisteAn(liste);
+		
+		String datenPfad = "./Elektronikrechnungen_Daten.json";
+		String schemaPfad = "./Elektronikrechnungen_Schema.json";
+	
+		SchemaValidierer validierer = new SchemaValidierer();
+		boolean istGueltig = validierer.validiereJsonSchema(datenPfad, schemaPfad);
+		
+		if(istGueltig)
+		{
+			JsonTester test1 = new JsonTester(datenPfad);
+			Rechnungsliste liste1 = test1.leseJSONinRechnungsliste();
+			test1.zeigeRechnungslisteAn(liste1);
+//			Auskommentiert lassen --> mehrmaliges ueberschreiben
+//			liste.getRechnungen().add(test.erzeugeRechnung());
+//			test.schreibeRechnungslisteInJSON(liste, datenPfad);
+		}
+		else
+		{
+			System.out.println("Fehler: Die Rechnungsliste wurde nicht eingelesen- da Schema ungueltig!");
+		}
+	}
+}
