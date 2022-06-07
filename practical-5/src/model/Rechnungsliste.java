755526876047bf67214ff6b95385f0136a65321d
fhildeb <felix@lukso.io>
2022-06-07 23:34:32 +0200

    model: add list

---
+++ practical-5/src/model/Rechnungsliste.java
@@ -0,0 +1,17 @@
+
+package model;
+
+import java.util.List;
+
+public class Rechnungsliste {
+
+	public List<Rechnung> rechnungen;
+
+	public List<Rechnung> getRechnungen() {
+		return rechnungen;
+	}
+
+	public void setRechnungen(List<Rechnung> rechnungen) {
+		this.rechnungen = rechnungen;
+	}
+}
